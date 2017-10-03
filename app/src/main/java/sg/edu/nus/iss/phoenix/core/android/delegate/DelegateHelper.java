dd0409dbf448ababa9a4883adc410c8a00bf9287
Jack Le <e0146768@u.nus.edu>
2017-10-03 23:23:03 +0800

    Server change

--- app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
+++ app/src/main/java/sg/edu/nus/iss/phoenix/core/android/delegate/DelegateHelper.java
@@ -2,12 +2,11 @@ package sg.edu.nus.iss.phoenix.core.android.delegate;
 
 public class DelegateHelper {
 
-    public final static String SERVER = "http://172.17.249.149:8080";
+    public final static String SERVER = "http://172.23.195.183:8080";
     // RESTful parameters.
     public final static String PRMS_BASE_URL_AUTHENTICATE = SERVER + "/phoenix/rest/Login/doLogin?";
     public final static String PRMS_BASE_URL_RADIO_PROGRAM = SERVER + "/phoenix/rest/radioprogram";
     public final static String PRMS_BASE_URL_USER = SERVER + "/phoenix/rest/maintainuser";
-
 
     // "http://10.0.3.2:9000/phoenix/rest/programslot"; - genymotion local
     // 10.202.16.89 - pp in mentorica  "http://10.202.16.89:15856/phoenix/rest/programslot"
